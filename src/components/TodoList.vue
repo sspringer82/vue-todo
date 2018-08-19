c87caf159c1d761a832e383e18cc5333714e3d77
Sebastian Springer <info@sebastian-springer.com>
2018-08-19 07:26:29 +0200

    initial

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <TodoListItem v-for="todo in todos" v-bind:todo="todo" 
+     v-bind:key="todo.id"></TodoListItem>
+  </ul>
+</template>
+
+
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: { TodoListItem },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: 'Get up', done: true },
+        { id: 2, title: 'Eat', done: true },
+        { id: 3, title: 'Sleep', done: false },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding-left: 0;
+  width: 400px;
+  margin: 0 auto;
+}
+</style>
+
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li v-bind:class="todo.done ? 'done' : 'open'">
+      {{todo.title}}
+      <span v-if="todo.done" v-on:click="toggleState(todo.id)">✔</span>
+      <span v-else @click="toggleState(todo.id)">✘</span>
+    </li>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+};
+</script>
+
+<style scoped>
+li {
+  line-height: 30px;
+  border: 1px solid darkgray;
+  padding: 10px;
+  margin: 2px 0;
+  position: relative;
+}
+span {
+  position: absolute;
+  right: 10px;
+}
+
+.done {
+  background-color: lightgreen;
+}
+
+.open {
+  background-color: lightsalmon;
+}
+</style>
